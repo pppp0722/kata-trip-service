0d41b85fb65eb6439f56426fa21b8f2594dfc7b8
ilhwan <poj0722@naver.com>
2023-01-30 14:45:16 +0900

    :white_check_mark: isFriend 테스트 추가

--- src/test/java/com/example/tripservicekata/user/UserTest.java
+++ src/test/java/com/example/tripservicekata/user/UserTest.java
@@ -10,18 +10,42 @@ public class UserTest {
 
     @Test
     public void 여행_정보를_추가하고_불러올_수_있다() {
+        // given
         User user = new User();
         user.addTrip(new Trip());
+
+        // when
         List<Trip> trips = user.trips();
+
+        // then
         assertThat(trips.size()).isEqualTo(1);
     }
 
     @Test
     public void 친구_정보를_추가하고_불러올_수_있다() {
+        // given
         User user = new User();
         user.addFriend(new User());
+
+        // when
         List<User> friends = user.getFriends();
+
+        // then
         assertThat(friends.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void 사용자의_친구인지_확인할_수_있다() {
+        // given
+        User user = new User();
+        User friend = new User();
+        user.addFriend(friend);
+
+        // when
+        boolean isFriend = user.isFriend(friend);
+
+        // then
+        assertThat(isFriend).isEqualTo(true);
     }
 
 }
